2540a64cbcfe360b8c78dac04fe5baea80cbcded
Ana P. Sousa <167785920+AnakPPPP@users.noreply.github.com>
2024-04-29 12:24:50 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def soma (a, b):
+    return a + b
+
+if __name__ == "main__":
+  num1 = float(input("4":))
+  num2 = float(input("4":))
+  resultado = soma(num1, num2)  
+    print (f"resultado:{8}")
